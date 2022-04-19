61626c4313fc8f6ca7ca94f2fb4c4de6d59849d7
Sebastian Ornelas <sebastianornelas@berkeley.edu>
2022-04-18 22:45:51 -0700

    hw8 complete

---
+++ routes/shop.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { check, validationResult} = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const auth = require("./../middleware/auth");
+
+const User = require("../model/User");
+
+
+router.get('/list', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        const list = user.shoppinglist;
+        res.send(list);
+    } catch (e) {
+      res.send({ message: 'Error in Fetching user' });
+    }
+});
+
+router.post('/add', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        const list = user.shoppinglist;
+        list.push(req.body.item);
+        user.shoppinglist = list;
+        await user.save();
+        res.send(list);
+    } catch (e) {
+        res.send({ message: 'Error in adding element' });
+    }
+});
+
+router.delete('/delete', auth, async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        if (user == null) {
+            return res.send(200).json({message: "lol"});
+        }
+        const list = user.shoppinglist;
+        const item = req.body.item;
+        //remove this item from array 
+        let index = list.indexOf(item);
+        while (index !== -1) {
+            list.splice(index, 1);
+            index = list.indexOf(item);
+        }
+        user.shoppinglist = list;
+        await user.save();
+        res.send(list);
+    } catch (e) {
+        res.send({ message: 'Error in deleting element' });
+    }
+});
+//arr = arr.filter(e => e !== 'B');
+
+module.exports = router;
